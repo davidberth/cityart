20d8ce1b09ff338873d0e4c505d622efd1a3e8b5
David Berthiaume <david@papercrane.io>
2021-10-23 21:01:03 -0400

    finished implementation of vector based generation.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 from shapely.geometry import Point, LineString
 from shapely.ops import unary_union
 
-size = (1000, 1000)
+size = (2000, 2000)
 size_half = (size[0] / 2, size[1] / 2)
 
 surface = cairo.ImageSurface(cairo.FORMAT_RGB24, *size)
@@ -62,24 +62,24 @@ union = unary_union(paths)
 print (' done')
 
 # grab the end points to determine the intersections
-# we can use just the
-points = [c for l in union for c in l.coords]
-points = np.array(points)
+nodes = [c for l in union for c in l.coords]
+nodes = np.array(nodes)
 
-print (points.shape)
-# now we remove the duplicate points
-points = np.unique(points, axis=0)
-
-print (points.shape)
+# now we remove the duplicate points and retrieve the counts
+# the counts are the degrees of each node
+nodes, counts = np.unique(nodes, axis=0, return_counts=True)
 
 rgb  = np.random.random((3))
 
-for k in zip(points):
-    x = k[0][0]
-    y = k[0][1]
-    ctx.arc(x,y,0.024,0,math.pi*2)
-    ctx.set_source_rgba(*rgb, 0.3)
+for coord, count in zip(nodes, counts):
+    x = coord[0]
+    y = coord[1]
+
+    ctx.arc(x,y,0.0024*count,0,math.pi*2)
+    ctx.set_source_rgba(*rgb, 0.5)
     ctx.fill()
 
 surface.write_to_png('out/test1.png')
 os.system(r'out\test1.png')
+
+# generate the street lights
