ba90f1571de011bfd8a5bba957763e0f556d7874
David Berthiaume <david@papercrane.io>
2021-12-26 23:25:57 -0500

    Finished with top geometries and base heights for islands.  Will work on base geometries next.

--- city.py
+++ city.py
@@ -3,14 +3,13 @@ import random
 import os
 import math
 import numpy as np
-from shapely.geometry import Point, LineString, Polygon
+from shapely.geometry import Point, LineString, Polygon, MultiPoint
 from shapely.ops import unary_union
 import colorsys
 from params import *
 from tqdm import tqdm
 import trimesh
 import pyrender
-import sys
 
 size_half = (size[0] / 2, size[1] / 2)
 
@@ -62,23 +61,34 @@ def perform_union(paths):
     nodes, counts = np.unique(nodes, axis=0, return_counts=True)
     return nodes, counts
 
-def render_building(angle1, angle2, radius, outer_radius, buildings, height_offset):
+def render_building(x, y, max_radius, height_offset, building_geoms):
+    dist = np.sqrt(x ** 2 + y ** 2)
+    angle = math.atan2(x,y)
+    xn, yn = x / dist, y / dist
+    # grab the orthogonal direction
+    xo, yo = yn, -xn
+    random_rate = dist * 0.004 + 0.002
+    random_rate_half = random_rate / 2
+    forw_random = random.random() * random_rate - random_rate_half
+    perp_random = random.random() * random_rate - random_rate_half
+    forw_distance = 0.005 + forw_random
+    perp_distance = 0.005 + perp_random
 
-    x1 = np.cos(angle1) * radius
-    y1 = np.sin(angle1) * radius
-    x2 = np.cos(angle2) * radius
-    y2 = np.sin(angle2) * radius
-
-    x3 = np.cos(angle2) * outer_radius
-    y3 = np.sin(angle2) * outer_radius
-    x4 = np.cos(angle1) * outer_radius
-    y4 = np.sin(angle1) * outer_radius
+    x1 = x - xn * forw_distance - xo * perp_distance
+    y1 = y - yn * forw_distance - yo * perp_distance
+    x2 = x - xn * forw_distance + xo * perp_distance
+    y2 = y - yn * forw_distance + yo * perp_distance
+    x3 = x + xn * forw_distance + xo * perp_distance
+    y3 = y + yn * forw_distance + yo * perp_distance
+    x4 = x + xn * forw_distance - xo * perp_distance
+    y4 = y + yn * forw_distance - yo * perp_distance
 
     ctx.move_to(x1, y1)
     ctx.line_to(x2, y2)
     ctx.set_line_width(0.001)
-    hue = random.random() / 7.0 + 0.5 * angle1 / np.pi
-    sat = 1.0 - (random.random() / 7.0 + 1.1 * radius)
+
+    hue = random.random() / 7.0 + 0.5 * angle / np.pi
+    sat = 1.0 - (random.random() / 7.0 + 1.1 * dist)
     val = random.random() / 2.0 + 0.5
     rgb = colorsys.hsv_to_rgb(hue, sat, val)
 
@@ -97,12 +107,19 @@ def render_building(angle1, angle2, radius, outer_radius, buildings, height_offs
     coords = [(x1,y1), (x2,y2), (x3,y3), (x4,y4)]
     edges = [(0, 1), (1, 2), (2, 3), (3, 0)]
     poly = trimesh.path.polygons.edges_to_polygons(edges, np.array(coords))[0]
-    mesh = trimesh.creation.extrude_polygon(poly, (1.0 - radius) * building_height_rate + random.random() * 0.02)
+    mesh = trimesh.creation.extrude_polygon(poly, (1.0 - dist) * building_height_rate + random.random() * 0.02 - 0.01)
     # translate the mesh to lay on the terrain
-    mesh.vertices[:, 2] += (1.0 - radius) * terrain_height_rate - 0.008 + height_offset
+    base_height = terrain_get_base_height(max_radius, height_offset) + terrain_get_extrude_height(max_radius, dist)
+    mesh.vertices[:, 2] += base_height
     mesh.visual.mesh.visual.face_colors = [255, 0, 64, 200]
-    buildings.append(mesh)
+    building_geoms.append(mesh)
 
+def terrain_get_base_height(island_max_radius, island_height_offset):
+    return 0.8 * (1.0 - island_max_radius) * terrain_height_rate + island_height_offset
+
+def terrain_get_extrude_height(island_max_radius, radius):
+    radius_difference = island_max_radius - radius
+    return radius_difference * terrain_height_rate + 0.001
 
 # render the branch starting at the root
 # our path list
@@ -143,15 +160,16 @@ print ('done')
 islands = list(circle_union.geoms)
 island_height_offsets = []
 for island in islands:
-    print (island.centroid, island.area)
     island_height_offset = random.random() * 0.1
     island_height_offsets.append(island_height_offset)
 
 # generate the inner circles
 inner_circles = []
 meshes = []
+island_max_radius = np.zeros(len(islands))
+
 print ('generating inner ring geometries')
-for radius in tqdm(np.arange(inner_circle_min, inner_circle_max, inner_circle_increment)):
+for radius in tqdm(np.arange(inner_circle_max, inner_circle_min, -inner_circle_increment)):
 
     point = Point(0,0)
     circle = point.buffer(radius, resolution = 8)
@@ -178,78 +196,87 @@ for radius in tqdm(np.arange(inner_circle_min, inner_circle_max, inner_circle_in
             ctx.set_source_rgb(radius, radius, 1.0)
             ctx.stroke()
 
-        for island_index in range(len(island_height_offsets)):
+        for island_index in range(len(islands)):
             if islands[island_index].intersects(line):
                 break
 
-
+        if island_max_radius[island_index] < radius:
+            island_max_radius[island_index] = radius
         # create the ring geometries
         edges = np.array([np.arange(0, len(coords)-1), np.arange(1, len(coords))]).T
         edges[-1, 1] = 0
         poly = trimesh.path.polygons.edges_to_polygons(edges, np.array(coords))[0]
-        mesh = trimesh.creation.extrude_polygon(poly, 0.5 * (1.0 - radius) * terrain_height_rate)
-        mesh.vertices[:, 2] += 0.5 * (1.0 - radius) * terrain_height_rate - 0.001 + island_height_offsets[island_index]
-        mesh.visual.mesh.visual.face_colors = [64, 128, 64, 200]
+
+        base_height = terrain_get_base_height(island_max_radius[island_index], island_height_offsets[island_index])
+        extrude_height = terrain_get_extrude_height(island_max_radius[island_index], radius)
+
+        mesh = trimesh.creation.extrude_polygon(poly, extrude_height)
+        mesh.vertices[:, 2] += base_height
+        mesh.visual.mesh.visual.face_colors = [222, 222, 222, 255]
         meshes.append(mesh)
 
 
-# generate the building rings
-circle_index = 1
+# generate the building candidates
 print ('\ndone')
-print ('generating the building rings')
-buildings = []
-for inner_radius in tqdm(np.arange(inner_circle_min, inner_circle_max - inner_circle_increment, inner_circle_increment)):
+print ('generating building candidates')
+building_points = []
+for inner_radius in np.arange(inner_circle_max, inner_circle_min, -inner_circle_increment):
 
-    #print (f'generating building ring {inner_radius}')
     number_of_buildings = int(inner_radius * building_rate)
     number_of_buildings += random.randint(0, int(number_of_buildings * building_rate_upper_factor))
     angle_offset = random.random() * angle_offset_factor
-
     angle_inc = 2.0 * np.pi / number_of_buildings
-    building_half_angle = angle_inc * building_width_max
-    inner_circle = inner_circles[circle_index]
+
     for angle_original in np.arange(0.0, np.pi * 2.0 - 0.0001, angle_inc):
         angle = angle_original + angle_offset
-        x = np.cos(angle) * (inner_radius + 0.0125)
-        y = np.sin(angle) * (inner_radius + 0.0125)
-        point = Point(x,y)
-        if inner_circle.contains(point):
 
-            if random.random() > 0.65 + inner_radius:
-                outer_radius = inner_radius + 0.02
-                x1 = np.cos(angle) * inner_radius
-                y1 = np.sin(angle) * inner_radius
-                x2 = np.cos(angle) * outer_radius
-                y2 = np.sin(angle) * outer_radius
-                ctx.move_to(x1, y1)
-                ctx.line_to(x2, y2)
-                ctx.set_line_width(0.001)
-                ctx.set_source_rgb(1.0,1.0,1.0)
-                ctx.stroke()
+        if random.random() > 0.65 + inner_radius:
+            outer_radius = inner_radius + 0.02
+            x1 = np.cos(angle) * inner_radius
+            y1 = np.sin(angle) * inner_radius
+            x2 = np.cos(angle) * outer_radius
+            y2 = np.sin(angle) * outer_radius
+            ctx.move_to(x1, y1)
+            ctx.line_to(x2, y2)
+            ctx.set_line_width(0.001)
+            ctx.set_source_rgb(1.0,1.0,1.0)
+            ctx.stroke()
 
-            elif random.random() > inner_radius * 0.6:
+        elif random.random() > inner_radius * 0.6:
+            x = np.cos(angle) * (inner_radius + 0.0125)
+            y = np.sin(angle) * (inner_radius + 0.0125)
+            point = Point(x, y)
+            building_points.append(point)
 
-                # determine the island offset of this point
-                for island_index in range(len(islands)):
-                    if islands[island_index].contains(point):
-                        break
-
-                # render a building
-                outer_radius = inner_radius + 0.017 - random.random()*.005
-                radius = inner_radius + 0.003 + random.random()*.005
-                angle1 = angle - building_half_angle * (random.random()*0.5 + 0.5)
-                angle2 = angle + building_half_angle * (random.random()*0.5 + 0.5)
-                height_offset = island_height_offsets[island_index]
-                render_building(angle1, angle2, radius, outer_radius, buildings, height_offset)
-
-    circle_index+=1
 print('\ndone')
+
+# intersect the building candidates with the islands
+building_geoms = []
+building_points = MultiPoint(building_points)
+e = 0
+print ('intersecting islands with building candidates')
+for island in tqdm(islands):
+    #print (f'intersecting island {e + 1} with building candidates')
+    intersection = island.intersection(building_points)
+    if intersection:
+        max_radius = island_max_radius[e]
+        height_offset = island_height_offsets[e]
+        # loop through the intersected buildings and add them to our geometries
+        if isinstance(intersection, MultiPoint):
+            point_list = intersection.geoms
+        else:
+            point_list = MultiPoint([intersection])
+
+        for i in point_list:
+            x,y = i.coords[0]
+            render_building(x,y, max_radius, height_offset, building_geoms)
+    e+=1
 
 for island in islands:
     x, y = island.centroid.coords[0]
-    ctx.arc(x,y, 0.001, 0.0, math.pi*2.0)
+    ctx.arc(x,y, 0.007, 0.0, math.pi*2.0)
     ctx.set_source_rgb(1.0, 1.0, 1.0)
-    ctx.set_line_width(0.007)
+    ctx.set_line_width(0.003)
     ctx.stroke()
 
 for node in nodes:
@@ -267,10 +294,10 @@ for node in nodes:
 surface.write_to_png(out_image)
 os.system(out_image)
 terrain_concat = trimesh.util.concatenate(meshes)
-building_concat = trimesh.util.concatenate(buildings)
+building_concat = trimesh.util.concatenate(building_geoms)
 
-scene = trimesh.scene.Scene(geometry=[terrain_concat, building_concat])
-trimesh.exchange.export.export_scene(scene, 'out/out.glb', file_type='glb')
+#scene = trimesh.scene.Scene(geometry=[terrain_concat, building_concat])
+#trimesh.exchange.export.export_scene(scene, 'out/out.glb', file_type='glb')
 pyrender_terrain = pyrender.Mesh.from_trimesh(terrain_concat, smooth=False)
 pyrender_building = pyrender.Mesh.from_trimesh(building_concat, smooth=False)
 
--- params.py
+++ params.py
@@ -10,7 +10,7 @@ influence_radius_min = 0.018
 influence_radius_max = 0.12
 influence_circle_resolution = 8
 
-inner_circle_min = 0.025
+inner_circle_min = 0.005
 inner_circle_max = 0.98
 inner_circle_increment = 0.02
 inner_circle_noise_factor = 0.009
@@ -20,7 +20,7 @@ building_rate_upper_factor = 0.25
 angle_offset_factor = 0.1
 building_width_max = 0.35
 
-terrain_height_rate = 0.1
+terrain_height_rate = 0.2
 building_height_rate = 0.03
 
 out_image = r'out\out.png'
